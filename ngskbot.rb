2b0a88257115506017df54e22fdea7f5b5231fc3
e115763 <e115763@gmail.com>
2012-03-08 20:17:38 +0900

    add Twitter auth

--- ngskbot.rb
+++ ngskbot.rb
@@ -2,30 +2,45 @@
 # -*- coding: utf-8 -*-
 
 require 'pp'
-require 'user_stream'
 require 'thread'
+require 'user_stream'
+require 'twitter'
 require './message_check.rb'
 
-UserStream.configure do |config|
-  config.consumer_key = gets.chomp
-  config.consumer_secret = gets.chomp
-  config.oauth_token = gets.chomp
-  config.oauth_token_secret = gets.chomp
-end
-
+# 標準入力からトークン、botname,username(複数可)を取得
+consumer_key = gets.chomp
+consumer_secret = gets.chomp
+oauth_token = gets.chomp
+oauth_token_secret = gets.chomp
 bot_name = gets.chomp
 user_name = []
-check = Message_check.new bot_name,user_name
-
-
 while true
   tmp = gets.chomp
   break if tmp.empty?
   user_name.push tmp
 end
 
+# UserStreamとRESTに認証
+UserStream.configure do |config|
+  config.consumer_key = consumer_key
+  config.consumer_secret = consumer_secret
+  config.oauth_token = oauth_token
+  config.oauth_token_secret = oauth_token_secret
+end
+Twitter.configure do |config|
+  config.consumer_key = consumer_key
+  config.consumer_secret = consumer_secret
+  config.oauth_token = oauth_token
+  config.oauth_token_secret = oauth_token_secret
+end
+
+# チェック用クラス生成
+check = Message_check.new bot_name,user_name
+
+# UserStream 作成
 client = UserStream.client
 
+# Thread 間通信用キュー作成
 q = Queue.new
 
 #UserStreamを受信するスレッドを作成
@@ -38,9 +53,16 @@ user_stream = Thread.new do
   end
 end
 
+# スレッドを走らせる
 user_stream.run
 
 while true
-  #キューが空ならスレッドは停止する
+  # キューが空ならスレッドは停止するので、無限ループしてても大丈夫
   pp check.format_check q.pop
+
+  # error が出たら落ちちゃうので、begin-rescue-end でゴリ押し
+  begin
+    Twitter.update("test now")
+  rescue
+  end
 end
