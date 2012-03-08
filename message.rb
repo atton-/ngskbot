19c189daa85fe5a69e5f89b6318bb1647f41a2c0
e115763 <e115763@gmail.com>
2012-03-08 14:13:04 +0900

    add count_botname

--- message.rb
+++ message.rb
@@ -4,6 +4,7 @@
 # メッセージをどう処理するのか決めるクラス
 # botの名前とかoriginal_userの名前を入れておく
 
+
 class Message
 
   def initialize bot_name,original_name
@@ -12,19 +13,25 @@ class Message
   end
 
   def test text
-    has_usrname? text
+     count_botname text
   end
 
   def formatCheck tweet
     # フォーマットをチェックする
     # 返り値と処理
     # 0       追加
-    # -1      エラー : 改行が含まれている
-    # -2      エラー : フォーマットが違う
+    # -1      エラー : クライアントが夜フクロウじゃない
+    # -2      エラー : 改行が含まれている
+    # -3      エラー : フォーマットが違う
     # 1以上   数字の分だけ返信
-    
-    if has_usrname? tweet.text
-      
+
+
+    # original_usernameが含まれていない場合
+    # 通常返信なので、bot_nameが含まれている数を返す
+    if !has_usrname? tweet.text do
+      return count_botname tweet.text
+    end
+
     end
 
   end
@@ -39,6 +46,17 @@ class Message
     false
   end
 
+  def count_botname text
+    # bot_name の数を数える
+    # bot_name が無くなるまで置換を繰り返し、繰り返しの数を個数として返す
+    i = 0
+    while text != text.sub("@#{@bot_name}","")
+      text.sub!("@#{@bot_name}","")
+      i += 1
+    end
+    i
+  end
+
 end
 
 bot = "ngskbot"
@@ -47,3 +65,5 @@ users =["hamilton___","daijoubjanai","daijoubujanee"]
 puts Message.new(bot,users).test "hoge"
 puts Message.new(bot,users).test "@hamilton___"
 puts Message.new(bot,users).test "@daijoubujanee"
+puts Message.new(bot,users).test "@ngskbot"
+puts Message.new(bot,users).test "@ngskbot@ngskbot@ngskbot@ngskbot"
